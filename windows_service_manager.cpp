99ce0f115c246a2d35e089b339492ea24d3c6b60
AliGhaffarian <alighaffarian9@gmail.com>
2023-10-06 15:20:08 +0330

    log rollback ignoring not running services

--- windows_service_manager.cpp
+++ windows_service_manager.cpp
@@ -4,9 +4,17 @@
 #include <vector>
 #include <fstream>
 #include "json-develop/single_include/nlohmann/json.hpp"
-
+#include <regex>
+#include <ctime>
+#include <sstream>
+#include <iomanip>
+#include <filesystem>
 
 using json = nlohmann::json;
+
+//intersect
+//log
+//rollback based on log
 
 class WindowsService {
 public:
@@ -18,6 +26,9 @@ public:
     	for (auto& displayName : this->displayName){
     		std::cout << displayName << std::endl;
 		}
+	}
+	bool operator ==(const WindowsService &second){
+		return this->serviceName == second.serviceName;
 	}
 };
 
@@ -70,6 +81,7 @@ int UserConfirmWindowsServiceAction(const WindowsService& service, ServiceAction
 int LoadFromJson(std::vector<WindowsService> &WindowsServices, std::string fileName = "services.json"){
 	// Load data from JSON file
     std::ifstream jsonFile(fileName);
+	
     if (!jsonFile.is_open()) {
         std::cerr << "Error opening JSON file." << std::endl;
         return 1;
@@ -100,6 +112,61 @@ int LoadFromJson(std::vector<WindowsService> &WindowsServices, std::string fileN
     return 0;
 }
 
+std::string LoadFromTextFile(std::string lileName){
+	std::ifstream file(lileName);
+
+	if (file.is_open() == false){
+		std::cout << "problem reading result of sc query";
+		return std::string();
+	}
+		
+
+	std::string loadedLine;
+	std::string wholeString;
+
+	while(std::getline(file, loadedLine))
+		wholeString += loadedLine + "\n";
+
+	file.close();
+
+	return wholeString;
+
+}
+
+std::vector<std::string> ActiveServiceNames(std::string fileName = "QueryResult.txt"){
+	if (system("servicequery") != 0){
+		std::cout << "\n-------------------\nproblem running servicequery.bat\nchecking if services are running or not is not possible\ncontinue? (y/n)";
+		char response;
+		std::cin >> response;
+		if (response == 'n' || response == 'N')
+			exit(0);
+	}
+	
+	std::string loadedText;
+	std::vector<std::string> result;
+	if ((loadedText = LoadFromTextFile(fileName)) == std::string())
+		return std::vector<std::string>();
+
+	std::regex pattern("SERVICE_NAME: ([^\\r\\n]+)");
+
+    // Iterator to search for matches
+    std::sregex_iterator iterator(loadedText.begin(), loadedText.end(), pattern);
+    std::sregex_iterator end;
+
+    // Loop through the matches and extract SERVICE_NAME values
+    while (iterator != end) {
+        std::smatch match = *iterator;
+        result.push_back(match[1].str());
+        
+
+        // Move to the next match
+        ++iterator;
+    }
+	return result;
+}
+
+
+
 int RollBack(const std::vector<WindowsService> &services){
 	for(auto &service : services){
 		WindowsServiceAction(service, ServiceAction::Enable);
@@ -116,6 +183,8 @@ void PrintHelp(){
 					"Be carefull out there!" << std::endl;
 }
 
+
+
 void ArgsHandler(std::string arg, const std::vector<WindowsService> &WindowsServices){
 	
 	if (arg == "rollback"){
@@ -131,27 +200,108 @@ void ArgsHandler(std::string arg, const std::vector<WindowsService> &WindowsServ
 	}
 }
 
+int ServicePurge(std::vector<WindowsService> &windowsServices){
+	std::vector<std::string> activeWindowsServiceNames = ActiveServiceNames();
+	int removedTimes = 0;
+	
+	
+	
+	for (size_t i = 0 ; i < windowsServices.size() ; i++)
+	{
+		for(size_t j = 0 ; j < windowsServices[i].serviceName.size() ; j++){
+			
+			if (
+			std::find(activeWindowsServiceNames.begin(), 
+							activeWindowsServiceNames.end(), 
+							windowsServices[i].serviceName[j]) 
+				== activeWindowsServiceNames.end()){
+								
+				windowsServices.erase(windowsServices.begin() + i);
+				
+				i = 0;
+				removedTimes += 1;
+			}
+		}
+	}
+	
+	return removedTimes;
+	
+}
+
+std::string GetLogFileName() {
+    std::time_t tt = std::time(nullptr);
+    std::tm* ti = std::localtime(&tt);
+    char buffer[80];
+    std::strftime(buffer, sizeof(buffer), "%Y-%m-%d", ti);
+
+    std::filesystem::path logDir("log");
+    if (!std::filesystem::exists(logDir))
+        std::filesystem::create_directory(logDir);
+
+    std::ostringstream fileNameStream;
+    fileNameStream << logDir.string() << "\\" << buffer;
+
+    std::string baseFileName = fileNameStream.str();
+    std::string fileName = baseFileName;
+    int counter = 0;
+
+    while (std::filesystem::exists(fileName + ".txt")) {
+        counter++;
+        std::ostringstream newFileNameStream;
+        newFileNameStream << baseFileName << "_" << counter;
+        fileName = newFileNameStream.str();
+    }
+
+    fileName += ".txt"; // Append the file extension
+
+    return fileName;
+}
+
+std::ofstream CreateLogFile(){
+	std::string fileName = GetLogFileName();
+	std::cout << fileName;
+	system("pause");
+    std::ofstream log(fileName);
+
+    if (!log.is_open()) {
+        std::cerr << "Problem opening log file: " << fileName << std::endl;
+		std::cerr << "Error code: " << errno << std::endl;
+        // Handle the error here, e.g., throw an exception or return an empty stream
+		system("dir");
+		system("pause");
+    }
+
+    return log;
+}
+
 int main(int argc, char ** args) {
 	
-	std::vector<WindowsService> WindowsServices;
-
-    if (LoadFromJson(WindowsServices) == 1)
+	std::vector<WindowsService> windowsServices;
+	if (LoadFromJson(windowsServices) == 1)
     	return 1;
 	
 	if (argc > 1){
-		ArgsHandler(args[1], WindowsServices);
+		ArgsHandler(args[1], windowsServices);
 	}
+	
+	system("cls");
+	
+	CreateLogFile();
 	
 	
 	PrintHelp();
 	system("pause");
 	system("cls");
 
-	std::cout << "rollback? y/n";
+	std::cout << ServicePurge(windowsServices) << " services were not running and skipped" << std::endl;
+	system("pause");
+	system("cls");
+	
+	std::cout << "rollback previous changes? y/n";
 	char input;
 	std::cin >> input;
 	if(input == 'Y' || input == 'y'){
-		RollBack(WindowsServices);
+		RollBack(windowsServices);
 		system("pause");
 		exit(0);
 	}
@@ -159,7 +309,7 @@ int main(int argc, char ** args) {
 	system("cls");	
 
     // Loop through WindowsServices and ask for confirmation to disable
-	for (const auto& service : WindowsServices) {
+	for (const auto& service : windowsServices) {
         int result = UserConfirmWindowsServiceAction(service, ServiceAction::Disable);
         if (result == 0) {
             std::cout << "Service disabled successfully." << std::endl;
